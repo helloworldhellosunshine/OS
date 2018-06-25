cf9cddef3931aed6c468209270243c64441ca217
王帝 <1443005893@qq.com>
2018-06-25 16:47:32 +0800

    这个是CPU函数进行了改进

--- app/src/main/java/com/example/os/PCB.java
+++ app/src/main/java/com/example/os/PCB.java
@@ -9,9 +9,11 @@ public class PCB {
     public static final int STATUS_BLOCK = 1;   // 阻塞
     public static final int STATUS_EXECUTE = 2; // 执行
     public int id; // 进程标识符
-    public String IR;    // IR寄存器  存储指令
-    public int variables;  // 中间变量
-    public int PSW;    // 程序状态寄存器
     public int time;   // 阻塞时间
+    public int variables;  // 中间变量
+
+    //寄存器的模拟
+    public String IR;    // IR寄存器  存储指令
+    public int PSW;    // 程序状态寄存器
     public int PC;     // PC寄存器 存储将要读取的指令地址
 }
